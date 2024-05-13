cda76c0d6b833481c00b1505aa8db9e4d4db4d28
AntonioPaess <paesdeandradeantonio@gmail.com>
2024-05-13 16:23:37 -0300

    Adicionando def random e receitas já pré-selecionadas no bloco de notas.

--- Projeto.py
+++ Projeto.py
@@ -1,3 +1,6 @@
+import random
+
+
 def menu():
     tam = 30
     opcoes = {
@@ -21,9 +24,6 @@ def menu():
 
         op = input("=> ")
 
-        if op not in opcoes:
-            print("Opção Inválida")
-            continue
         if op == "0":
             break
         elif op == "1":
@@ -39,16 +39,59 @@ def menu():
         elif op == "6":
             pass
         elif op == "7":
-            pass
+            print(receita_aleatoria())
         elif op == "8":
             pass
+        else:
+            if op not in opcoes:
+                print("Opção Inválida")
+                continue
 
-
-        print(f"{opcoes.get(op)}\n")
+        # print(f"{opcoes.get(op)}\n")
 
 
 
 def cadastro():
+    pass
+
+
+
+
+def favoritos():
+    pass
+
+def receita_aleatoria():
+    with open('dados.txt', 'r', encoding='utf-8') as arquivo:
+        linhas = arquivo.readlines()
+        receitas = []
+        nova_receita = False
+        for linha in linhas:
+            if nova_receita:
+                ingredientes = linha.strip().split(', ')
+                nova_receita = False
+            elif ' - ' in linha:
+                receitas.append(linha.split(' - ', 1))
+                nova_receita = True
+        receita_escolhida = random.choice(receitas)
+        indice = linhas.index(receita_escolhida[0] + ' - ' + receita_escolhida[1])
+        ingredientes = []
+        for linha in linhas[indice + 1:]:
+            if not linha.strip():
+                break
+            ingredientes.append(linha.strip())
+        indice_modo_preparo = indice + len(ingredientes) + 1
+        modo_preparo = ''
+        for linha in linhas[indice_modo_preparo + 1:]:
+            if not linha.strip():
+                break
+            modo_preparo += linha.strip() + '\n'
+        print("Receita:", receita_escolhida[1].strip())
+        for ingrediente in ingredientes:
+            print()
+            print("- " + ingrediente)
+        print()
+        print(modo_preparo)
+
 
 
 
